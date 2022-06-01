6aa82f7cf5ce67b172eb246ff4b6a015e6ea96b6
Borja Garcia <91528745+Borja-garcia98@users.noreply.github.com>
2022-06-01 10:34:50 +0200

    Add files via upload

---
+++ interactivo/script.js
@@ -0,0 +1,28 @@
+var vid = document.getElementById("myVideo");
+
+function setCurTime10() {
+    vid.currentTime = 10;
+}
+function setCurTime15() {
+    vid.currentTime = 15;
+}
+function setCurTime20() {
+    vid.currentTime = 20;
+}
+function setCurTime25() {
+    vid.currentTime = 25;
+}
+function setCurTime45() {
+    vid.currentTime = 45;
+}
+function setCurTime50() {
+    vid.currentTime = 50;
+}
+function setCurTime55() {
+    vid.currentTime = 55;
+}
+function setCurTime59() {
+    vid.currentTime = 60;
+}
+
+
